7b8d98751449367a271d7e805184643097cc4734
Brian Sanchez <rcbriansanchez@gmail.com>
2025-01-09 04:20:56 -0800

    login implemented

--- database.py
+++ database.py
@@ -53,15 +53,15 @@ def handle_database_task(data):
 def database_acc_register(data, connection):
     print("Registering account")
 
-    # Validate username
     username = data["username"]
     if not (username.isalnum() or "_" in username or "-" in username):
         return "0_con_0001"
-
     password = data["password"]
     if not all(char.isalnum() or char in "!@#$%^&*" for char in password):
         return "0_con_0002"
 
+    # Encrypt the password
+    # encrypted_password = fernet.encrypt(password.encode()).decode()
     encrypted_password = "hash"
     account_data = {
         "username": username,
@@ -87,7 +87,39 @@ def database_acc_register(data, connection):
             ),
         )
         connection.commit()
-        return "1"  # Registration successful
+        return "1"
+    except mariadb.Error as e:
+        print(f"Database error: {e}")
+        return f"0_db_{str(e.errno)}"
+    finally:
+        cursor.close()
+
+
+def database_acc_login(data, connection):
+    print("Logging in account")
+    identifier = data["identifier"]
+    password = data["password"]
+
+    try:
+        cursor = connection.cursor()
+
+        query = """
+        SELECT JSON_EXTRACT(data, '$.password')
+        FROM accounts
+        WHERE email = ? OR JSON_EXTRACT(data, '$.username') = ?
+        """
+        cursor.execute(query, (identifier, identifier))
+        result = cursor.fetchone()
+
+        if result:
+            print(f"this is the result: {result}")
+            stored_password = result[0].strip('"')
+            if stored_password == password:
+                return "1"
+            else:
+                return "0_db_0002"
+        else:
+            return "0_db_0003"
     except mariadb.Error as e:
         print(f"Database error: {e}")
         return f"0_db_{str(e.errno)}"
